af9992f28ab7fd719060578ab652e5dee090f49e
Taras Halaz <taras.halaz@gmail.com>
2020-01-29 17:26:53 +0200

    header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import {
-    BrowserRouter as Router,
-    Route,
-    Link
+    BrowserRouter as Router, Route, Link
 } from 'react-router-dom';
+import HeaderComponent from "./pages/header/component";
 import MenusPageComponent from './pages/menus-page/component';
-import OrderPageComponent from './pages/order-page/component';
 import MenuPageComponent from './pages/menu-page/component';
+import OrderPageComponent from './pages/order-page/component';
 import DeliveryPageComponent from './pages/delivery-page/component';
 import AppContextProvider from './appContext';
 
@@ -17,26 +16,11 @@ function App() {
             <AppContextProvider>
                 <Router>
                     <div>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <Link to="/">Menus</Link>
-                                </li>
-                                <li>
-                                    <Link to="/menu/:menuId">Menu items</Link>
-                                </li>
-                                <li>
-                                    <Link to="/order">Order</Link>
-                                </li>
-                                <li>
-                                    <Link to="/delivery">Delivery</Link>
-                                </li>
-                            </ul>
-                        </nav>
+                        <HeaderComponent />
                         <Route exact path="/">
                             <MenusPageComponent />
                         </Route>
-                        <Route path="/menu">
+                        <Route path="/menu/:menuId">
                             <MenuPageComponent />
                         </Route>
                         <Route path="/order">
