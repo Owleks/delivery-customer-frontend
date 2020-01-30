75e0d973543a717e48d48ef6f5ac0d65a5906f63
Taras Halaz <taras.halaz@gmail.com>
2020-01-30 19:03:19 +0200

    added 'Restaurant not found' handler

--- src/appContext.js
+++ src/appContext.js
@@ -1,5 +1,19 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { getRestaurantId } from './common/components/actions';
+import { makeStyles, Box } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  centered: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+}));
 
 export const AppContext = createContext({
   menus: [],
@@ -9,11 +23,13 @@ export const AppContext = createContext({
   restaurantId: undefined,
 });
 const AppContextProvider = ({ children }) => {
+  const classes = useStyles();
   const [menus, setMenus] = useState([]);
   const [menu, setMenu] = useState([]);
   const [orders, setOrders] = useState({});
   const [isBasketDialogOpened, setIsBasketDialogOpened] = useState(false);
   const [restaurantId, setRestaurantId] = useState(undefined);
+  const [errorMessage, setErrorMessage] = useState(undefined);
   const context = {
     setMenus,
     setMenu,
@@ -40,14 +56,15 @@ const AppContextProvider = ({ children }) => {
         setRestaurantId(restaurantId);
       })
       .catch(e => {
-        console.error(e); // TODO: error handling
+        setErrorMessage('Restaurant not found');
       });
   }, []);
 
   return (
     <>
       <AppContext.Provider value={context}>
-        {children}
+        {!errorMessage && children}
+        {errorMessage && <Box className={classes.centered}><b>{errorMessage}</b></Box>}
       </AppContext.Provider>
     </>
   );
