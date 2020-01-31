7bd54243ec7f75059434228d7dbca003077858b5
Taras Halaz <taras.halaz@gmail.com>
2020-01-31 11:38:21 +0200

    Basket dialogue: small fixes, added navigation to '/' page

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,8 @@ function App() {
                         <Route path="/delivery" component={DeliveryPageComponent}>
                         </Route>
                     </div>
+                    <BasketDialog />
                 </Router>
-                <BasketDialog />
             </AppContextProvider>
         </>
     );
--- src/common/components/basket-dialog/component.jsx
+++ src/common/components/basket-dialog/component.jsx
@@ -1,105 +1,108 @@
-import React, {memo, useContext, useState} from 'react';
-import {AppContext} from '../../../appContext';
+import React, { memo, useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { AppContext } from '../../../appContext';
 import {
-    Grid, Button, Dialog, DialogContent,
-    DialogTitle, Box, IconButton, Input, InputLabel, FormControl, FormGroup,
+  Grid, Button, Dialog, DialogContent,
+  DialogTitle, Box, IconButton, Input, InputLabel, FormControl, FormGroup,
 } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import {placeOrder} from '../actions';
+import { placeOrder } from '../actions';
 
 const BasketDialog = memo((props) => {
-    const initialState = {
-        customerName: '',
-        phoneNumber: '',
-        address: '',
-        time: '',
-        description: '',
-    };
-    const context = useContext(AppContext);
-    const isOpened = context.isBasketDialogOpened;
-    const [form, setForm] = useState(initialState);
-    const onFormChange = (name) => ({target}) => {
-        const field = {};
-        field[name] = target.value;
-        setForm({
-            ...form,
-            ...field
-        });
-    };
+  const initialState = {
+    customerName: '',
+    phoneNumber: '',
+    address: '',
+    time: '',
+    description: '',
+  };
+  const context = useContext(AppContext);
+  const history = useHistory();
+  const isOpened = context.isBasketDialogOpened;
+  const [form, setForm] = useState(initialState);
+  const onFormChange = (name) => ({ target }) => {
+    const field = {};
+    field[name] = target.value;
+    setForm({
+      ...form,
+      ...field,
+    });
+  };
 
-    const onDialogClose = () => {
-        context.setIsBasketDialogOpened(false);
-    };
-    const onOrderButtonClick = () => {
-        const orders = Object.keys(context.orders).map((id) => {
-            return {
-                menuItemId: id,
-                count: context.orders[id]
-            }
-        });
-        placeOrder({
-            ...{restaurantId: context.restaurantId},
-            ...form,
-            ...{items: orders}
-        }).then(() => {
-            context.setOrders({});
-            context.setIsBasketDialogOpened(false);
-            setForm(initialState);
-        });
+  const onDialogClose = () => {
+    context.setIsBasketDialogOpened(false);
+  };
+  const onOrderButtonClick = () => {
+    const orders = Object.keys(context.orders).map((id) => {
+      return {
+        menuItemId: id,
+        count: context.orders[id],
+      };
+    });
+    placeOrder({
+      ...{ restaurantId: context.restaurantId },
+      ...form,
+      ...{ items: orders },
+    }).then(() => {
+      context.setOrders({});
+      context.setIsBasketDialogOpened(false);
+      setForm(initialState);
+      history.push('/');
+    });
 
-    };
+  };
 
-    return (
-        <>
-            <Dialog fullScreen open={isOpened}>
-                <Grid container justify="flex-end">
-                    <IconButton aria-label="close" onClick={onDialogClose}>
-                        <CloseIcon />
-                    </IconButton>
-                </Grid>
-                <Grid container justify="center">
-                    <DialogTitle id="form-dialog-title">
-                        Login
-                    </DialogTitle>
-                </Grid>
+  return (
+    <>
+      <Dialog fullScreen open={isOpened}>
+        <Grid container justify="flex-end">
+          <IconButton aria-label="close" onClick={onDialogClose}>
+            <CloseIcon />
+          </IconButton>
+        </Grid>
+        <Grid container justify="center">
+          <DialogTitle id="form-dialog-title">
+            Delivery details
+          </DialogTitle>
+        </Grid>
 
-                <DialogContent>
-                    <FormGroup>
-                        <FormControl>
-                            <InputLabel htmlFor="name">Name</InputLabel>
-                            <Input onChange={onFormChange('customerName')} value={form.customerName} fullWidth
-                                   id="name" />
-                        </FormControl>
-                        <FormControl>
-                            <InputLabel htmlFor="Phone">Password</InputLabel>
-                            <Input onChange={onFormChange('phoneNumber')} value={form.phoneNumber} fullWidth
-                                   id="phone" />
-                        </FormControl>
-                        <FormControl>
-                            <InputLabel htmlFor="address">address</InputLabel>
-                            <Input onChange={onFormChange('address')} value={form.address} fullWidth id="address" />
-                        </FormControl>
-                        <FormControl>
-                            <InputLabel htmlFor="time">Time</InputLabel>
-                            <Input onChange={onFormChange('time')} value={form.time} fullWidth id="time" />
-                        </FormControl>
-                        <FormControl>
-                            <InputLabel htmlFor="comment">Comment</InputLabel>
-                            <Input onChange={onFormChange('description')} value={form.description} fullWidth
-                                   id="comment" />
-                        </FormControl>
-                    </FormGroup>
-                    <Box m={2}>
-                        <Grid container justify="center">
-                            <Button fullWidth onClick={onOrderButtonClick} variant="contained" color="primary">
-                                Order!11
-                            </Button>
-                        </Grid>
-                    </Box>
-                </DialogContent>
-            </Dialog>
-        </>
-    );
+        <DialogContent>
+          <FormGroup>
+            <FormControl>
+              <InputLabel htmlFor="name">Name</InputLabel>
+              <Input onChange={onFormChange('customerName')} value={form.customerName} fullWidth
+                     id="name" />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="Phone">Phone</InputLabel>
+              <Input onChange={onFormChange('phoneNumber')} value={form.phoneNumber} fullWidth
+                     id="phone" />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="address">Address</InputLabel>
+              <Input onChange={onFormChange('address')} value={form.address} fullWidth id="address" />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="time">Delivery time</InputLabel>
+              <Input onChange={onFormChange('time')} value={form.time} fullWidth id="time" />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="comment">Comment</InputLabel>
+              <Input onChange={onFormChange('description')} value={form.description} fullWidth
+                     id="comment" />
+            </FormControl>
+          </FormGroup>
+          <Box m={2}>
+            <Grid container justify="center">
+              <Button fullWidth onClick={onOrderButtonClick} variant="contained" color="primary">
+                Order!11
+              </Button>
+            </Grid>
+          </Box>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 });
 
 
